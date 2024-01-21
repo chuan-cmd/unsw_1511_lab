023da59af7456b58c451350b1dc473cb4c33d526
sudu cheong <141207585+chuan-cmd@users.noreply.github.com>
2024-01-21 14:39:59 +1100

    Week 1

---
+++ messy.c
@@ -0,0 +1,11 @@
+// COMP1511 Lab01 Exercise - meesy
+// Authors:
+// Cheong Zhuang(z5512751@ad.unsw.edu.au)
+// Written:15/01/2024 
+//This is a program to teach me how to use style function,how to make comments.
+
+#include <stdio.h>
+int main(void){
+    printf("It is VERY inportant to program with good style\n");
+    return 0;
+}
